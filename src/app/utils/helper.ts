5cf25f73b7b758f5ab5f0191b7edf2df1ae349dd
Rodrigo Papadopulo <rpapadopulo.2106@gmail.com>
2019-06-03 00:25:02 -0300

    Sacado debugger del helper

--- src/app/utils/helper.ts
+++ src/app/utils/helper.ts
@@ -3,7 +3,6 @@ import { Injectable } from "@angular/core";
 @Injectable()
 export class Helper { 
     public static getLocaleDate(date: Date){
-        debugger;
         let dia = date.getDate();
         let anho = date.getFullYear();
         let mes = date.getMonth() + 1;
